7e307d1755cb87027801a6a8964045b9345efc17
Aniket <rajroyaniket@gmail.com>
2025-04-06 13:51:40 +0530

    font adjustment

--- 05_passwordgenerator/src/App.jsx
+++ 05_passwordgenerator/src/App.jsx
@@ -34,7 +34,7 @@ function App() {
   }, [length, numberAllowed, charAllowed, passwordGenerator]);
 
   return (
-    <div className="w-full max-w-md mx-auto shadow-md rounded-lg px-4 py-3 my-8 bg-gray-800 text-orange-500">
+    <div className="md:w-[40vw] text-2xl mx-auto shadow-md rounded-lg px-4 py-3 my-8 bg-gray-800 text-orange-500 mt-[35vh]">
       <h1 className="text-white text-center my-3">Password Generator</h1>
       <div className="flex shadow rounded-lg overflow-hidden mb-4">
         <input
