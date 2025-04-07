ad403218810ebda430dd5ef553df75b16fae5b5e
Aniket <rajroyaniket@gmail.com>
2025-04-07 23:53:22 +0530

    currency convertor

---
+++ 06_CurrencyConvertor/src/App.jsx
@@ -0,0 +1,16 @@
+
+import React from 'react'
+
+
+function App() {
+  return (
+    <>
+      <h1 className="text-4xl text-white flex justify-center mt-3.5">
+        Currency Convertor
+      </h1>
+
+    </>
+  )
+}
+
+export default App
