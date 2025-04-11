d179f109503bd617deaf51ec2699e237dc06075b
Aniket <rajroyaniket@gmail.com>
2025-04-11 23:10:49 +0530

    Adding funtionality

--- 06_CurrencyConvertor/src/hooks/usecurrencyinfo.js
+++ 06_CurrencyConvertor/src/hooks/usecurrencyinfo.js
@@ -5,8 +5,13 @@ function useCurrencyInfo(currency) {
     const [data, setData] = useState({})
 
     useEffect(() => {
-        fetch(`https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${currency}.json`)
+        fetch(`https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/${currency}.json`)
+        // res = reponse
         .then((res) => res.json())
         .then((res) => setData(res[currency]))
     },[currency])
+    return data
 }
+
+export default useCurrencyInfo;
+
